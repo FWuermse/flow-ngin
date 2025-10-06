efe63aec6801a4a3772fbef904968f259abe2fd9
Florian Wuermseer <wuermseer.florian@gmail.com>
2025-10-06 12:28:48 +0200

    fix: add wasm logic for asset-loading

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -44,6 +44,17 @@ pub fn diffuse_normal_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
     })
 }
 
+#[cfg(target_arch = "wasm32")]
+fn format_url(file_name: &str) -> reqwest::Url {
+    let window = web_sys::window().unwrap();
+    let location = window.location();
+    let mut origin = location.origin().unwrap();
+    if !origin.ends_with("learn-wgpu") {
+        origin = format!("{}/assets", origin);
+    }
+    let base = reqwest::Url::parse(&format!("{}/", origin,)).unwrap();
+    base.join(file_name).unwrap()
+}
 
 pub async fn load_string(file_name: &str) -> anyhow::Result<String> {
     #[cfg(target_arch = "wasm32")]
