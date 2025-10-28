ba0651a4edb4d2483a0b84616f57982d4d2f8ec8
Florian Wuermseer <wuermseer.florian@gmail.com>
2025-10-28 22:38:17 +0100

    chore: don't run the game until the caller invokes flow::run()

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 pub mod camera;
 pub mod context;
 pub mod data_structures;
+pub mod flow;
 pub mod pick;
 pub mod pipelines;
+pub mod render_pass;
 pub mod resources;
-pub mod flow;
-
-fn run_flows() {
-    flow::run().unwrap();
-}
