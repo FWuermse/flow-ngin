b8998359536d81c65af4080ba650505275d7837e
Florian Wuermseer <wuermseer.florian@gmail.com>
2026-03-10 00:30:16 +0100

    feat: card ui component

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,8 +3,10 @@ pub mod text_label;
 pub mod image;
 pub mod container;
 pub mod background;
+pub mod card;
 
 pub use image::{HAlign, VAlign};
 pub use container::Container;
 pub use layout::{Layout, UIElement};
 pub use background::{Background, BackgroundTexture};
+pub use card::Card;
